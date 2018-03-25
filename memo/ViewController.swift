59d81b69d0bef35280ce5c0ce3f5b02a01958f6f
박기찬 <pkch0102@naver.com>
2018-03-25 12:51:43 +0900

    메모 프로젝트 생성

---
+++ memo/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  memo
+//
+//  Created by 박기찬 on 2018. 3. 25..
+//  Copyright © 2018년 박기찬. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var tableview: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ memo/memolistCell.swift
@@ -0,0 +1,15 @@
+//
+//  memolistCell.swift
+//  memo
+//
+//  Created by 박기찬 on 2018. 3. 25..
+//  Copyright © 2018년 박기찬. All rights reserved.
+//
+
+import UIKit
+
+class memolistCell: UITableViewCell{
+    
+    @IBOutlet weak var memolabel: UILabel!
+    
+}
